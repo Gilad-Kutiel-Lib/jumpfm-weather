4fee0b04a0cffdae41eb8a2259b7379d6b3c45fd
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-14 16:39:35 +0300

    init

---
+++ index.ts
@@ -0,0 +1,48 @@
+import * as weather from 'weather-js'
+
+interface CurrentWeather {
+    temperature: number
+    skycode: number
+    skytext: string
+    date: string
+    observationtime: string
+    observationpoint: string
+    feelslike: number
+    humidity: number
+    winddisplay: string
+    day: string
+    shortday: string
+    windspeed: string
+    imageUrl: string
+}
+
+export function load(jumpFm) {
+    function update() {
+        weather.find({
+            search: jumpFm.settings.getStr('weatherLocation', 'Haifa, IL'),
+            degreeType: 'C'
+        },
+            (err, res) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+
+                const cur = res[0].current as CurrentWeather
+                const temp = cur.temperature
+                const cls =
+                    temp < 25 ? 'info' : (
+                        temp < 35 ? 'warn' :
+                            'err'
+                    )
+
+                jumpFm.statusBar.msg([cls])(
+                    'weather',
+                    `${cur.temperature}° ${cur.humidity}% ${cur.windspeed.replace(/ /g, '')}`
+                )
+            })
+    }
+
+    update()
+    setInterval(update, 36000)
+}
